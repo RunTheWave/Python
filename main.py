20fb5a8aff2d59fe76f1c8546d6bd60243ef84f1
RunTheWave <60888152+RunTheWave@users.noreply.github.com>
2020-02-13 10:15:11 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
-answer = "y"
-counter = 1
-totalaverage = 0
-while answer == "y":
-  total = 0
-  highest = 0
-  lowest = 10
-  for s in range(5):
-    score = float(input("Enter the score:"))
-    while score < 0 or score > 10:
-      print "Invaild Score, try again"
-      score = float(input("Enter the score:"))
-    if score > highest:
-      highest = score
-    if score < lowest:
-      lowest = score
-    total += score
-  print highest, lowest
-  average = (total - highest - lowest) / 3
-  totalaverage += average
-  finalaverage = totalaverage / counter
-  print average
-  answer = input("another user? y/n")
-  print "times tried:", counter
-  counter = counter + 1
-print finalaverage
+ask = input("Are you encrypting or decrypting? Enter 'e' or 'd'")
+
+string = input("Enter message!")
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+newstring = ""
+
+key = int(input("Enter Key"))
+
+
+i = 0
+while i < len(string):
+  letter = string[i]
+  whereisletter = alphabet.find(letter)
+  if ask == 'e':
+    newletter = alphabet[(whereisletter + key)%26]
+  else:
+    newletter = alphabet[(whereisletter - key)%26]
+  newstring = newstring+newletter
+  i+=1
+print newstring
+
